bab77932f52f579304917772e9d7c1b4c4ddf907
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-10-21 23:55:42 +0530

    Day 117 Completed

---
+++ day-117-Split-the-array(gfg).cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int mod = 1e9+7;
+        int xr = 0;
+        int n = arr.size();
+        
+        for(int i=0; i<n; i++){
+            xr ^= arr[i];
+        }
+        
+        if(xr != 0) return 0;
+        
+        int ans = 1;
+        for(int i=0; i<n-1; i++){
+            ans = (ans*2)% mod;
+        }
+        return ans -1;
+    }
+};
