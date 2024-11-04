137502580d58ba0fd2d92da86973a733989ff4f8
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-11-04 17:21:40 +0530

    Day 130 Completed Successfully

---
+++ day-130-Find-all-triplets-with-zero-sum(gfg).cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &arr) {
+        // Code here
+        set<vector<int>> reSet;
+        unordered_map<int, vector<pair<int, int>>> mp;
+        
+        int n= arr.size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                mp[arr[i] + arr[j]].push_back({i,j});
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(mp.count(-arr[i])){
+                for(auto &p: mp[-arr[i]]){
+                    if(p.first == i || p.second == i) continue;
+                    
+                    vector<int> curr = {p.first, p.second, i};
+                    sort(begin(curr), end(curr));
+                    reSet.insert(curr);
+                }
+            }
+        }
+        
+        vector<vector<int>> res(begin(reSet), end(reSet));
+        return res;
+    }
+};
