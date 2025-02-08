44955ba28c08cfa0224e085be02964524d4309fe
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-02-09 00:00:58 +0530

    Rervision

---
+++ Learn the Basics/Basic Maths/Palindrome-Number(leetcode).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string nums = to_string(x);
+        int low = 0, high = nums.size()-1;
+        while(low <= high){
+            if(nums[low] == nums[high]){
+                low++;
+                high--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
