0d0389860618d0ea274f3894c012a3a4176ecc07
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-02-21 23:59:51 +0530

    DSA Revival Day 3 Complete

---
+++ Learn the Basics/Basic Recusion/demo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+int solve(vector<int>& ar, int target)
+{
+
+
+}
+
+int main()
+{
+    vector<int> ar{1,2};
+    int target = 5;
+
+    int ans = solve(ar, target);
+    cout<<"Answer: "<<ans<<endl;
+    return 0;
+}
